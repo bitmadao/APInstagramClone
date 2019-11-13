c54b97b5d83a5f542eb073dec6d0c94083402b67
bitmadao <dag@pepmagic.no>
2019-11-13 15:58:58 +0100

    refactor for use of single register button to submit boxer or kickboxer. make switch at top toggle whether boxer or kickboxer is registered.

--- app/src/main/java/com/udemy/apinstagramclone/SignUp.java
+++ app/src/main/java/com/udemy/apinstagramclone/SignUp.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -18,11 +19,11 @@ import com.parse.SaveCallback;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener, Switch.OnCheckedChangeListener{
 
-    private Button btnBoxer, btnKickBoxer;
+    private Button btnRegister;
     private Switch swClass;
 
     private TextInputEditText txtInpName, txtInpPunchSpeed, txtInpPunchPower, txtInpKickSpeed, txtInpKickPower;
-    private TextInputLayout txtInpLayKickSpeed, txtInpLayKickPower;
+    private LinearLayout lLaySignUpFormKickBoxerSpecific;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,68 +34,110 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener, S
         // Save the current Installation to Back4App
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
-        btnBoxer = findViewById(R.id.btnBoxer);
-        btnKickBoxer = findViewById(R.id.btnKickBoxer);
+        txtInpName = findViewById(R.id.txtInpName);
+        txtInpPunchSpeed = findViewById(R.id.txtInpPunchSpeed);
+        txtInpPunchPower = findViewById(R.id.txtInpPunchPower);
+        txtInpKickSpeed = findViewById(R.id.txtInpKickSpeed);
+        txtInpKickPower = findViewById(R.id.txtInpKickPower);
+
+        btnRegister = findViewById(R.id.btnRegister);
         swClass = findViewById(R.id.swClass);
 
+        lLaySignUpFormKickBoxerSpecific = findViewById(R.id.lLaySignUpFormKickBoxerSpecific);
 
-        txtInpLayKickSpeed = findViewById(R.id.txtInpLayKickSpeed);
-        txtInpLayKickPower = findViewById(R.id.txtInpLayKickPower);
+        lLaySignUpFormKickBoxerSpecific.setVisibility(View.GONE);
 
-        txtInpLayKickSpeed.setVisibility(View.GONE);
-        txtInpLayKickPower.setVisibility(View.GONE);
-
-        btnBoxer.setOnClickListener(SignUp.this);
-        btnKickBoxer.setOnClickListener(SignUp.this);
+        btnRegister.setOnClickListener(SignUp.this);
         swClass.setOnCheckedChangeListener(SignUp.this);
 
     }
 
-    public void btnBoxerTapped(){
-        ParseObject boxer = new ParseObject("Boxer");
+    public void registerBoxer(String name, int punchSpeed, int punchPower){
+        final ParseObject boxer = new ParseObject("Boxer");
 
-        boxer.put("punch_speed", 200);
+        boxer.put("name",name);
+        boxer.put("punch_speed",punchSpeed);
+        boxer.put("punch_power",punchPower);
+
         boxer.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
-
                 if(e == null){
-                    Toast.makeText(SignUp.this, "boxer object saved successfully", Toast.LENGTH_LONG).show();
+                    Toast.makeText(SignUp.this, boxer.get("name") +" object saved successfully! (Boxer)", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(SignUp.this, e.toString(), Toast.LENGTH_LONG).show();
                 }
             }
         });
-    }    public void btnKickBoxerTapped(){
+    }
+
+    public void registerKickBoxer(String name, int punchSpeed, int punchPower, int kickSpeed, int kickPower){
         final ParseObject kickBoxer = new ParseObject("KickBoxer");
 
-        kickBoxer.put("name","Tim");
-        kickBoxer.put("punch_speed", 200);
-        kickBoxer.put("punch_power", 30);
-        kickBoxer.put("kick_speed",300);
-        kickBoxer.put("kick_power",60);
+        kickBoxer.put("name", name);
+        kickBoxer.put("punch_speed", punchSpeed);
+        kickBoxer.put("punch_power", punchPower);
+        kickBoxer.put("kick_speed",kickSpeed);
+        kickBoxer.put("kick_power",kickPower);
+
         kickBoxer.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
-
                 if(e == null){
-                    Toast.makeText(SignUp.this, kickBoxer.get("name")+ " object saved successfully", Toast.LENGTH_LONG).show();
+                    Toast.makeText(SignUp.this, kickBoxer.get("name") + " object saved successfully! (KickBoxer)", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(SignUp.this, e.toString(), Toast.LENGTH_LONG).show();
                 }
             }
         });
+
+    }
+
+    public void btnRegisterTapped(){
+        if(txtInpName.getText().toString().isEmpty()){
+            Toast.makeText(SignUp.this, "We need at least a name to register..", Toast.LENGTH_LONG).show();
+        } else {
+            String name = txtInpName.getText().toString();
+            int punchSpeed = 2000;
+            int punchPower = 200;
+
+            if(!txtInpPunchSpeed.getText().toString().isEmpty()){
+                punchSpeed = Integer.parseInt(txtInpPunchSpeed.getText().toString());
+            }
+
+            if (!txtInpPunchPower.getText().toString().isEmpty()){
+                punchPower = Integer.parseInt(txtInpPunchPower.getText().toString());
+            }
+
+
+
+            if(!swClass.isChecked()){
+
+                registerBoxer(name,punchSpeed,punchPower);
+
+            } else {
+                int kickSpeed = 3000;
+                int kickPower = 300;
+
+                if(!txtInpKickSpeed.getText().toString().isEmpty()){
+                    kickSpeed = Integer.parseInt(txtInpKickSpeed.getText().toString());
+                }
+
+                if(!txtInpKickPower.getText().toString().isEmpty()){
+                    kickPower = Integer.parseInt(txtInpKickPower.getText().toString());
+                }
+
+                registerKickBoxer(name,punchSpeed,punchPower,kickSpeed,kickPower);
+            }
+        }
     }
 
     @Override
     public void onClick(View v) {
 
         switch (v.getId()){
-            case R.id.btnBoxer:
-                btnBoxerTapped();
-                break;
-            case R.id.btnKickBoxer:
-                btnKickBoxerTapped();
+            case R.id.btnRegister:
+                btnRegisterTapped();
                 break;
         }
     }
@@ -103,12 +146,12 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener, S
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if(isChecked){
             buttonView.setText(R.string.switch_class_name_kickboxer);
-            txtInpLayKickSpeed.setVisibility(View.VISIBLE);
-            txtInpLayKickPower.setVisibility(View.VISIBLE);
+            lLaySignUpFormKickBoxerSpecific.setVisibility(View.VISIBLE);
+            btnRegister.setText(R.string.btn_register_kickboxer);
         } else {
             buttonView.setText(R.string.switch_class_name_boxer);
-            txtInpLayKickSpeed.setVisibility(View.GONE);
-            txtInpLayKickPower.setVisibility(View.GONE);
+            lLaySignUpFormKickBoxerSpecific.setVisibility(View.GONE);
+            btnRegister.setText(R.string.btn_register_boxer);
         }
     }
 }
