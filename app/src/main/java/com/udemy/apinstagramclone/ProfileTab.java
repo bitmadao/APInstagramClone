9f339f9fa48c91ab0828d8e7a3feb9d0fd8c2f3c
bitmadao <dag@pepmagic.no>
2019-11-18 14:08:14 +0100

    update ProfileTab - Set EditTexts Enable to false if profile data exists, make update button set EditTexts Enable to true on click

--- app/src/main/java/com/udemy/apinstagramclone/ProfileTab.java
+++ app/src/main/java/com/udemy/apinstagramclone/ProfileTab.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
@@ -32,9 +33,12 @@ public class ProfileTab extends Fragment implements View.OnClickListener, Gestur
             edtTabProfileHobbies,
             edtTabProfileSport;
 
-    private Button btnTabProfileUpdateInfo;
+    private Button btnTabProfileUpdateInfo, btnTabProfileLogout;
 
     private ParseUser parseUser;
+
+    //state changes in btnTabProfileUpdateInfoTapped() and setTabProfileForm()
+    private boolean btnTabProfileUpdateInfoShouldUpdate = true;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -54,11 +58,18 @@ public class ProfileTab extends Fragment implements View.OnClickListener, Gestur
         edtTabProfileSport = profileTabView.findViewById(R.id.edtTabProfileSport);
 
         btnTabProfileUpdateInfo = profileTabView.findViewById(R.id.btnTabProfileUpdateInfo);
+        btnTabProfileLogout = profileTabView.findViewById(R.id.btnTabProfileLogout);
 
         parseUser = ParseUser.getCurrentUser();
         btnTabProfileUpdateInfo.setOnClickListener(ProfileTab.this);
+        btnTabProfileLogout.setOnClickListener(ProfileTab.this);
+        btnTabProfileLogout.setText(
+                String.format(getString(R.string.btn_tab_profile_logout),
+                parseUser.getUsername())
+            );
 
-        // TODO setTabProfileForm();
+
+        setTabProfileForm();
 
 
         // must return a view
@@ -70,32 +81,129 @@ public class ProfileTab extends Fragment implements View.OnClickListener, Gestur
         switch(v.getId()){
             case R.id.btnTabProfileUpdateInfo:
                 btnTabProfileUpdateInfoTapped();
+                break;
+
+            case R.id.btnTabProfileLogout:
+                btnTabProfileLogoutTapped();
+                break;
         }
 
 
     }
 
-    private void btnTabProfileUpdateInfoTapped(){
-        parseUser.put("profileName", edtTabProfileProfileName.getText().toString());
-        parseUser.put("profileBio", edtTabProfileBio.getText().toString());
-        parseUser.put("profileProfession", edtTabProfileProfession.getText().toString());
-        parseUser.put("profileHobbies", edtTabProfileHobbies.getText().toString());
-        parseUser.put("profileSport", edtTabProfileSport.getText().toString());
+    private void btnTabProfileUpdateInfoTapped() {
 
-        parseUser.saveInBackground(new SaveCallback() {
+
+
+        if(btnTabProfileUpdateInfoShouldUpdate) {
+            boolean somethingToUpdate = false;
+            if (!edtTabProfileProfileName.getText().toString().equals(parseUser.get("profileName").toString())) {
+
+                parseUser.put("profileName", edtTabProfileProfileName.getText().toString());
+                somethingToUpdate = true;
+
+            }
+
+            if (!edtTabProfileBio.getText().toString().equals(parseUser.get("profileBio").toString())) {
+                parseUser.put("profileBio", edtTabProfileBio.getText().toString());
+                somethingToUpdate = true;
+            }
+
+            if (!edtTabProfileProfession.getText().toString().equals(parseUser.get("profileProfession").toString())) {
+                parseUser.put("profileProfession", edtTabProfileProfession.getText().toString());
+            }
+
+            if (!edtTabProfileHobbies.getText().toString().equals(parseUser.get("profileHobbies").toString())) {
+                parseUser.put("profileHobbies", edtTabProfileHobbies.getText().toString());
+                somethingToUpdate = true;
+            }
+
+            if (!edtTabProfileSport.getText().toString().equals(parseUser.get("profileSport").toString())) {
+                parseUser.put("profileSport", edtTabProfileSport.getText().toString());
+                somethingToUpdate = true;
+            }
+
+            if(somethingToUpdate) {
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            FancyToast.makeText(getContext(),
+                                    getString(R.string.toast_tab_profile_profile_update_success),
+                                    FancyToast.LENGTH_LONG,
+                                    FancyToast.SUCCESS,
+                                    false)
+                                    .show();
+                            setTabProfileForm();
+
+                        } else {
+                            Log.i("AppTag", e.getMessage());
+                            FancyToast.makeText(getContext(),
+                                    e.getMessage(), // TODO make user friendly error
+                                    FancyToast.LENGTH_LONG,
+                                    FancyToast.ERROR,
+                                    false)
+                                    .show();
+                        }
+                    }
+                });
+            } else {
+                FancyToast.makeText(getContext(),
+                        "No changes made", // TODO add to strings.xml
+                        FancyToast.LENGTH_LONG,
+                        FancyToast.INFO,
+                        false)
+                    .show();
+                setTabProfileForm();
+            }
+        } else {
+            btnTabProfileUpdateInfoShouldUpdate = true;
+
+            if(parseUser.get("profileName") != null && !parseUser.get("profileName").toString().isEmpty()) {
+                edtTabProfileProfileName.setText(parseUser.get("profileName").toString());
+            }            
+            
+            if(parseUser.get("profileBio") != null && !parseUser.get("profileBio").toString().isEmpty()) {
+                edtTabProfileBio.setText(parseUser.get("profileBio").toString());
+            }            
+            
+            if(parseUser.get("profileProfession") != null && !parseUser.get("profileProfession").toString().isEmpty()) {
+                edtTabProfileProfession.setText(parseUser.get("profileProfession").toString());
+            }            
+            
+            if(parseUser.get("profileHobbies") != null && !parseUser.get("profileHobbies").toString().isEmpty()) {
+                edtTabProfileHobbies.setText(parseUser.get("profileHobbies").toString());
+            }            
+            
+            if(parseUser.get("profileSport") != null && !parseUser.get("profileSport").toString().isEmpty()) {
+                edtTabProfileSport.setText(parseUser.get("profileSport").toString());
+            }
+            edtTabProfileProfileName.setEnabled(true);
+
+            edtTabProfileBio.setEnabled(true);
+            edtTabProfileProfession.setEnabled(true);
+            edtTabProfileHobbies.setEnabled(true);
+            edtTabProfileSport.setEnabled(true);
+            btnTabProfileUpdateInfo.setText(R.string.btn_tab_profile_update_info);
+
+
+        }
+    }
+
+    private void btnTabProfileLogoutTapped() {
+        ParseUser.logOutInBackground(new LogOutCallback() {
             @Override
             public void done(ParseException e) {
-                if (e == null) {
+                // TODO add Logout stuff
+                if (e == null){
                     FancyToast.makeText(getContext(),
-                            "Profile update successful!", // TODO Profile-Update successful in strings
+                            "Logout Success",
                             FancyToast.LENGTH_LONG,
                             FancyToast.SUCCESS,
                             false)
                         .show();
-                    // TODO setTabProfileForm();
-
+                    getActivity().finish();
                 } else {
-                    Log.i("AppTag",e.getMessage());
                     FancyToast.makeText(getContext(),
                             e.getMessage(),
                             FancyToast.LENGTH_LONG,
@@ -106,19 +214,63 @@ public class ProfileTab extends Fragment implements View.OnClickListener, Gestur
             }
         });
     }
-
     private void setTabProfileForm() {
         if(parseUser.get("profileName") != null && !parseUser.get("profileName").toString().isEmpty()) {
-            edtTabProfileProfileName.setText(parseUser.get("profileName").toString());
+            btnTabProfileUpdateInfo.setText(R.string.btn_tab_profile_update_info_alternate);
+            btnTabProfileUpdateInfoShouldUpdate = false;
+
+            edtTabProfileProfileName.setText(
+                    String.format(getString(
+                            R.string.tab_profile_profile_name_txt_template),
+                            parseUser.get("profileName").toString()
+                    )
+                );
+
             edtTabProfileProfileName.setEnabled(false);
+            edtTabProfileBio.setEnabled(false);
+            edtTabProfileProfession.setEnabled(false);
+            edtTabProfileHobbies.setEnabled(false);
+            edtTabProfileSport.setEnabled(false);
+
+            if(parseUser.get("profileBio") != null ) {
+                edtTabProfileBio.setText(
+                        String.format(getString(
+                                R.string.tab_profile_bio_txt_template),
+                                parseUser.get("profileBio").toString()
+                        )
+                );
+
+            }
+
+            if(parseUser.get("profileProfession") != null) {
+                edtTabProfileProfession.setText(
+                        String.format(getString(
+                                R.string.tab_profile_profession_txt_template),
+                                parseUser.get("profileProfession").toString()
+                        )
+                );
+
+            }
+
+            if(parseUser.get("profileHobbies") != null) {
+                edtTabProfileHobbies.setText(
+                        String.format(getString(
+                                R.string.tab_profile_hobbies_txt_template),
+                                parseUser.get("profileHobbies").toString()
+                        )
+                );
+
+            }
+
+            if(parseUser.get("profileSport") != null) {
+                edtTabProfileSport.setText(
+                        String.format(getString(
+                                R.string.tab_profile_sport_name_txt_template),
+                                parseUser.get("profileSport").toString()
+                        )
+                );
+            }
         }
-/*
-        edtTabProfileProfileName
-        edtTabProfileBio
-        edtTabProfileProfession
-        edtTabProfileHobbies
-        edtTabProfileSport
-*/
     }
 
     @Override
