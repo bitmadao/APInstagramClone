969c1b5f16364d7db0c017f4a05412d0d04555d5
bitmadao <dag@pepmagic.no>
2019-11-19 10:48:15 +0100

    implement UsersTab - add ListView for ParseQuery of usernames

--- app/src/main/java/com/udemy/apinstagramclone/UsersTab.java
+++ app/src/main/java/com/udemy/apinstagramclone/UsersTab.java
@@ -8,6 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -15,6 +27,11 @@ import android.view.ViewGroup;
  */
 public class UsersTab extends Fragment {
 
+    private ListView listViewTabUsers;
+    private float listViewTabUsersAlpha;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
+    private TextView txtTabUsersLoadingUsers;
 
     public UsersTab() {
         // Required empty public constructor
@@ -25,7 +42,39 @@ public class UsersTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View usersTabView = inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        listViewTabUsers = usersTabView.findViewById(R.id.listViewTabUsers);
+        listViewTabUsersAlpha = listViewTabUsers.getAlpha();
+        listViewTabUsers.setAlpha(0);
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,arrayList);
+        txtTabUsersLoadingUsers = usersTabView.findViewById(R.id.txtTabUsersLoadingUsers);
+
+        populateUsersListView();
+
+        return usersTabView;
     }
 
+    private void populateUsersListView() {
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+
+
+        parseQuery.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e == null) {
+                    if(objects.size() > 0) {
+                        for(ParseUser user: objects){
+                            arrayList.add(user.getUsername());
+                        }
+                        listViewTabUsers.setAdapter(arrayAdapter);
+                        txtTabUsersLoadingUsers.animate().alpha(0).setDuration(2000).start();
+                        listViewTabUsers.animate().alpha(listViewTabUsersAlpha).setDuration(3000);
+                    }
+                }
+            }
+        });
+    }
 }
